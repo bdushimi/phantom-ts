cdc0f11317fa0cf69ed1df80f49d28f31c43f104
DUSHIMIMANA Bernard <35345818+bdushimi@users.noreply.github.com>
2020-07-07 16:29:43 +0200

    Resolve eslint errors in swagger.ts & updated the location of nodemon… (#1)
    * Resolve eslint errors in swagger.ts & updated the location of nodemon.json & swagger.ts & adds start scripts

---
+++ src/tests/UserController.test.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { expect } from 'chai';
+import 'mocha';
+import { agent as request } from 'supertest';
+import app from '../server';
+
+describe('User Controller', () => {
+  it('should return a Welcome message', async () => {
+    const res = await request(app).get('/api/');
+    expect(res.status).to.equal(200);
+    expect(res.body.response).to.equal('Welcome to Phantom');
+  });
+});
